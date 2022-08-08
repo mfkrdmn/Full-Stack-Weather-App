24d17c5c57870fec2fa34296470ac6d35194f19b
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-08 12:22:15 +0300

    last edits done

--- project/app/views.py
+++ project/app/views.py
@@ -16,12 +16,14 @@ def index(request):
             "country_code": str(list_of_data['sys']['country']),
             "coordinate": str(list_of_data['coord']['lon']) + ', '
             + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "temp": str(int(list_of_data['main']['temp'])) + ' °C',
             "pressure": str(list_of_data['main']['pressure']),
             "humidity": str(list_of_data['main']['humidity']),
             'main': str(list_of_data['weather'][0]['main']),
             'description': str(list_of_data['weather'][0]['description']),
             'icon': list_of_data['weather'][0]['icon'],
+            'wind' : str(list_of_data['wind']['speed']),
+            'cityname' : str(list_of_data['name']),
         }
         print(data)
     else:
